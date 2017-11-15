a576e1189c9ce9fe2094ffa235250fec7fc062a7
Sander Schutten <sander.schutten@live.nl>
2017-11-15 16:48:29 +0100

    Verify bug in UWP with ListView ContextActions using Prism's DelegateCommand

---
+++ ContextActionsBug/ContextActionsBug/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Prism.Commands;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace ContextActionsBug
+{
+    public partial class MainPage : ContentPage
+	{
+        public List<Person> Persons { get; } = new List<Person>
+            {
+                new Person{Name="Cartman"},
+                new Person{Name="Kenny"},
+                new Person{Name="Kyle"},
+                new Person{Name="Stan"}
+            };
+
+        public ICommand KillDelegateCommand => new DelegateCommand<Person>(Kill);
+        public ICommand KillCommand => new Command<Person>(Kill);
+
+        public MainPage()
+		{
+            InitializeComponent();
+            BindingContext = this;
+		}
+
+        private void Kill(Person person)
+        {
+            Debug.WriteLine($"Oh my God, they killed {person.Name}!");
+        }
+	}
+}
